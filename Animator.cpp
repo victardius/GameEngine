a32a3ea897f9a2af3f0ae683ef18619ba1257cba
victardius <37775040+victardius@users.noreply.github.com>
2018-12-14 11:28:39 +0100

    animator

--- Animator.cpp
+++ Animator.cpp
@@ -1,13 +1,25 @@
 #include "Animator.h"
+#include "GameController.h"
 
 namespace gameEngine {
 
-	Animator::Animator()
+	Animator::Animator(const char* sheetPath, int w, int h, int frames)
 	{
+		sf = IMG_Load(sheetPath);
+		tx = SDL_CreateTextureFromSurface(gc.getSys()->getRen(), sf);
+		rect = new SDL_Rect[frames];
+		SDL_QueryTexture(tx, NULL, NULL, &totSizeW, &totSizeH);
+		int x = 0, y = 0;
+		for (int i = 0; i < frames; i++) {
+			if ((y == 0 && w * i > totSizeW) || (y > 0 && w * i > totSizeW * y))
+				y++;
+			x = (w * i) - (totSizeW * y);
+			rect[i] = {x, y * h, w, h};
+		}
 	}
 
-	Animator* Animator::getInstance() {
-		return new Animator();
+	Animator* Animator::getInstance(const char* sheetPath, int w, int h, int frames) {
+		return new Animator(sheetPath, w, h, frames);
 	}
 
 	Animator::~Animator()
--- Animator.h
+++ Animator.h
@@ -1,15 +1,23 @@
 #ifndef ANIMATOR_H
 #define ANIMATOR_H
 
+#include <SDL.h>
+#include <SDL_image.h>
+
 namespace gameEngine {
 
 	class Animator
 	{
 	public:
-		Animator* getInstance();
+		Animator* getInstance(const char* sheetPath, int w, int h, int frames);
 		~Animator();
 	protected:
-		Animator();
+		Animator(const char* sheetPath, int w, int h, int frames);
+	private:
+		SDL_Surface* sf;
+		SDL_Texture* tx;
+		SDL_Rect* rect;
+		int totSizeW, totSizeH;
 	};
 
 }
